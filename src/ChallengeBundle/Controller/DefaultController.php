5cafd8a5a1bd81674cc2e99971b18ff4df2a2df1
mariemfaydi96 <mariem.faydi@esprit.tn>
2019-02-08 13:11:40 +0100

    projet

---
+++ src/ChallengeBundle/Controller/DefaultController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ChallengeBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('ChallengeBundle:Default:index.html.twig');
+    }
+    public function pageAction()
+    {
+        return $this->render('@Challenge/Default/page_une.html.twig');
+    }
+    public function layoutAction()
+    {
+        return $this->render('@Challenge/Layout/layout.html.twig');
+    }
+
+    public function challengeAction()
+    {
+        return $this->render('@Challenge/Layout/challenge.html.twig');
+    }
+}
